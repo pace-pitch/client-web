b812c76c2d099e3b93ef17cd3f3b59d2e57661d5
dinohan <dinohan.dev@gmail.com>
2024-05-08 10:11:45 +0900

    feat: add react-player

--- src/features/record/RecordAnalysis.tsx
+++ src/features/record/RecordAnalysis.tsx
@@ -1,5 +1,51 @@
+import { useRef, useState } from "react";
+import TestVideo from "../../assets/fps60.mp4";
 import styles from "./RecordAnalysis.module.scss";
+import ReactPlayer from "react-player";
 
 export function RecordAnalysis() {
-  return <div className={styles.wrapper}>RecordAnalysis</div>;
+  const videoRef = useRef<ReactPlayer>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [played, setPlayed] = useState(0);
+
+  const handleSeek = (time: number) => {
+    setPlayed(time);
+    if (videoRef.current) {
+      videoRef.current.seekTo(time);
+    }
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <ReactPlayer
+        playing={isPlaying}
+        onProgress={(progressProps) => {
+          setPlayed(progressProps.played);
+        }}
+        progressInterval={1000 / 30}
+        ref={videoRef}
+        width="100%"
+        height="auto"
+        url={TestVideo}
+        loop
+      />
+      <button
+        onClick={() => {
+          setIsPlaying(!isPlaying);
+        }}
+      >
+        재생
+      </button>
+      <input
+        type="range"
+        min={0}
+        max={1}
+        step={1 / 1_000_000}
+        value={played}
+        onChange={(e) => {
+          handleSeek(parseFloat(e.target.value));
+        }}
+      />
+    </div>
+  );
 }
