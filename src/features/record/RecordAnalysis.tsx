7b4d28b66726a50b6ef1ee065363fe96d29b1fa4
dinohan <dinohan.dev@gmail.com>
2024-06-10 05:07:46 +0900

    fix

--- src/features/record/RecordAnalysis.tsx
+++ src/features/record/RecordAnalysis.tsx
@@ -2,7 +2,9 @@ import { useRef, useState } from "react";
 import TestVideo from "../../assets/fps60.mp4";
 import styles from "./RecordAnalysis.module.scss";
 import ReactPlayer from "react-player";
-import { LineChart, Line, CartesianGrid } from "recharts";
+import { LineChart, Line, CartesianGrid, Legend, Tooltip } from "recharts";
+
+const X_WIDTH = 350;
 
 const data = [
   {
@@ -59,6 +61,33 @@ const data = [
   },
 ];
 
+const CustomTooltip = ({
+  active,
+  payload,
+}: {
+  active?: boolean;
+  payload?: {
+    name?: string;
+    value?: string;
+    dataKey?: string | number;
+    stroke?: string;
+  }[];
+}) => {
+  if (active && payload && payload.length) {
+    return (
+      <div>
+        {payload.map((p) => (
+          <p key={p.dataKey} style={{ color: p.stroke }}>
+            {p.name}: {p.value}
+          </p>
+        ))}
+      </div>
+    );
+  }
+
+  return null;
+};
+
 export function RecordAnalysis() {
   const videoRef = useRef<ReactPlayer>(null);
   const [isPlaying, setIsPlaying] = useState(false);
@@ -93,6 +122,9 @@ export function RecordAnalysis() {
         재생
       </button>
       <input
+        style={{
+          width: X_WIDTH,
+        }}
         type="range"
         min={0}
         max={1}
@@ -103,19 +135,22 @@ export function RecordAnalysis() {
           setIsPlaying(false);
         }}
       />
-
       <LineChart
-        width={393}
+        width={X_WIDTH}
         height={256}
         data={data}
         margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-        onClick={(e) => {
+        onMouseMove={(e) => {
           console.log(e);
+          const x = e.activeCoordinate?.x ?? 0;
+          handleSeek(x / X_WIDTH);
         }}
       >
+        <Legend verticalAlign="top" height={36} />
         <Line type="monotone" dataKey="root" stroke="#8884d8" dot={false} />
         <Line type="monotone" dataKey="pelvis" stroke="#82ca9d" dot={false} />
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" vertical={false} />
+        <Tooltip content={CustomTooltip} />
       </LineChart>
     </div>
   );
